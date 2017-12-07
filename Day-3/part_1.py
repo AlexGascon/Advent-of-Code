5dae6ca7c9bff96692c2c32d65388893da86038b
Alex <alexgascon.93@gmail.com>
2017-12-07 02:52:16 +0100

    Refactoring and adding comments to D3-P1 solution

--- Day-3/part_1.py
+++ Day-3/part_1.py
@@ -34,19 +34,20 @@ your puzzle input all the way to the access port?
 from math import sqrt, ceil
 
 
-def test_step_amount_required():
+"""TEST CASES"""
+def test_steps_to_center():
     # Test cases provided by the challenge description
-    assert step_amount_required(1) == 0
-    assert step_amount_required(12) == 3
-    assert step_amount_required(23) == 2
-    assert step_amount_required(1024) == 31
+    assert steps_to_center(1) == 0
+    assert steps_to_center(12) == 3
+    assert steps_to_center(23) == 2
+    assert steps_to_center(1024) == 31
     # Own test cases
-    assert step_amount_required(9) == 2
-    assert step_amount_required(25) == 4
-    assert step_amount_required(49) == 6
-    assert step_amount_required(4) == 1
-    assert step_amount_required(16) == 3
-    assert step_amount_required(36) == 5
+    assert steps_to_center(9) == 2
+    assert steps_to_center(25) == 4
+    assert steps_to_center(49) == 6
+    assert steps_to_center(4) == 1
+    assert steps_to_center(16) == 3
+    assert steps_to_center(36) == 5
 
 
 def test_next_odd_square():
@@ -82,7 +83,48 @@ def test_distance_from_corner():
     assert distance_from_corner(12) == 1
 
 
+"""FOLLOWED APPROACH
+
+If we draw a big-enough portion of the spiral:
+
+65  64  63  62  61  60  59  58  57
+    37  36  35  34  33  32  31  56
+.   38  17  16  15  14  13  30  55
+.   39  18   5   4   3  12  29  54
+.   40  19   6   1   2  11  28  53
+    41  20   7   8   9  10  27  52
+    42  21  22  23  24  25  26  51
+    43  44  45  46  47  48  49  50
+             .  .  .            81
+
+we'll be able to appreciate that the numbers that appear in the lower-left 
+diagonal are the squared values of the odd integers (9, 25, 49, 81...). Besides,
+due to the direction followed by the spiral, the values on this diagonal are 
+always the ones that complete a square. This has grants us a huge advantage:
+given any number, we can know in which layer it's located: we just need to find
+the immediately superior squared odd number.
+
+Knowing the value of this corner, we can also compute its distance to the center:
+it always corresponds to (its square root - 1) (i.e. 25 is 4 steps away, 9 is 2
+steps away, 81 is 8, etc). And this is very important, because the corners of 
+each layer have another interesting property: they are the farthest point in 
+that layer. Each step away from the corner that we take, no matter in which 
+direction (if we stay on the same layer, of course), will be a step closer to 
+the center.
+
+Therefore, there's an easy way to compute the distance to the center for every
+point: we just need to find the distance from the corners of that layer to the
+center, and the distance from that point to the closest corner. As we've said,
+each step away of the corner is a step towards the center, and therefore:
+
+d(point, center) = d(corner, center) - d(corner, point) 
+
+"""
+
+
+"""CODE"""
 def next_odd_square(x):
+    """Returns the immediately superior squared-odd-integer of a given number x"""
     closest_square_root = ceil(sqrt(x))
 
     if closest_square_root % 2 == 0:
@@ -92,27 +134,43 @@ def next_odd_square(x):
 
 
 def corner_to_center(corner):
+    """Computes the distance from a corner to the center of the spiral.
+    IMPORTANT: the given corner must be located on the lower-left diagonal
+    of the spiral"""
     return sqrt(corner) - 1
 
 
-def distance_from_corner(x):
-    next_corner = next_odd_square(x)
+def closest_corners(x):
+    """Finds out the value of the two corners closest to a given number (the
+    previous and the following one)"""
+
     side_length = sqrt(next_odd_square(x))
     distance_between_corners = side_length - 1
+
+    # We start on the lower left corner (the one we can find)
+    next_corner = next_odd_square(x)
     previous_corner = next_corner - distance_between_corners
 
+    # Then, we iterate until we found the ones surrounding the wanted number
     while previous_corner > x:
         next_corner, previous_corner = previous_corner, previous_corner - distance_between_corners
 
+    return next_corner, previous_corner
+
+
+def distance_from_corner(x):
+    """Calculates the distance to the closest corner"""
+    next_corner, previous_corner = closest_corners(x)
     return min(next_corner - x, x - previous_corner)
 
 
-def step_amount_required(x):
+def steps_to_center(x):
+    """Calculates the amount of steps to get to the center from a specific square"""
     closest_odd_square = next_odd_square(x)
 
-    return corner_to_center(closest_odd_square) - distance_from_corner(x)
+    return int(corner_to_center(closest_odd_square) - distance_from_corner(x))
 
 
 if __name__ == '__main__':
     input_value = 265149
-    print(step_amount_required(input_value))
+    print(steps_to_center(input_value))
