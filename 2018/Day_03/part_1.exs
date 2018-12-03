49af03ccc312be9b4d4d0aff27b735ff9a29522f
Alex Gascón <alex.gascon@coverwallet.com>
2018-12-03 19:52:14 +0100

    WIP

---
+++ 2018/Day_03/part_1.exs
@@ -0,0 +1,11 @@
+defmodule AdventOfCode2018.Day03.Part1 do
+  def overlapped_tiles do
+  end
+
+  defp parse_claim do
+  # #123 @ 3,2: 5x4 = [id: 123, left: 3, top: 2, right: 8, bottom: 6]
+  end
+
+  defp used_tiles(claim) do
+  end
+end
