3ae488309ecd998e11bab7c0a66a116fa8df6f04
Alex Gascón <alexgascon.93@gmail.com>
2018-12-04 02:33:03 +0100

    Part 2 solution

--- 2018/Day_03/part_1.exs
+++ 2018/Day_03/part_1.exs
@@ -1,55 +1,27 @@
 defmodule AdventOfCode2018.Day03.Part1 do
-  alias AdventOfCode2018.Utils
-  alias AdventOfCode2018.Day03.Parsing
+  alias AdventOfCode2018.Day03.Claims
   alias AdventOfCode2018.Day03.Fabric
 
   def overlapped_tiles(file_path) do
     file_path
-    |> Utils.read_lines!
-    |> Stream.map(&Parsing.parse_claim(&1))
-    |> Enum.reduce(%{}, fn claim, fabric -> Fabric.mark_occuped_space(claim, fabric) end)
+    |> Claims.extract
+    |> fabric_occupation
     |> Map.values
     |> Enum.count(fn occupation -> occupation > 1 end)
   end
+
+  def fabric_occupation(claims) do
+    claims
+    |> Enum.reduce(%{}, fn claim, fabric -> Fabric.mark_occuped_space(claim, fabric) end)
+  end
 end
 
-defmodule AdventOfCode2018.Day03.Parsing do
-  def parse_claim(claim) do
-    [
-      id:     claim |> parse_id,
-      left:   claim |> parse_left,
-      top:    claim |> parse_top,
-      width:  claim |> parse_width,
-      height: claim |> parse_height 
-    ]
-  end
-  defp parse_id(claim),     do: claim |> String.split("#") |> Enum.at(1) |> String.split |> Enum.at(0) |> String.to_integer
-  defp parse_left(claim),   do: claim |> String.split(",") |> Enum.at(0) |> String.split |> Enum.at(2) |> String.to_integer
-  defp parse_top(claim),    do: claim |> String.split(",") |> Enum.at(1) |> String.split(":") |> Enum.at(0) |> String.to_integer
-  defp parse_width(claim),  do: claim |> String.split("x") |> Enum.at(0) |> String.split |> Enum.at(3) |> String.to_integer
-  defp parse_height(claim), do: claim |> String.split("x") |> Enum.at(1) |> String.to_integer
-end
+ExUnit.start
 
-defmodule AdventOfCode2018.Day03.Fabric do
-  @fabric_side 1000
+defmodule AdventOfCode2018.Day03.Test1 do
+  use ExUnit.Case
 
-  def mark_occuped_space(claim, fabric) do
-    claim
-    |> occuped_space
-    |> Enum.reduce(fabric, fn tile, cloth -> Map.update(cloth, tile, 1, &(&1 + 1)) end)
-  end
+  import AdventOfCode2018.Day03.Part1
 
-  defp occuped_space(claim), do: occupy_space([], claim)
-
-  defp occupy_space(already_occuped, [id: _, left: _, top: _, width: _, height: 0]), do: already_occuped
-  defp occupy_space(already_occuped, [id: id, left: left, top: top, width: width, height: height]) do
-    first = tile_to_int(top, left)
-    last = tile_to_int(top, left + width - 1)
-    
-    Range.new(first, last)
-    |> Enum.concat(already_occuped)
-    |> occupy_space([id: id, left: left, top: top + 1, width: width, height: height - 1])
-  end
-
-  defp tile_to_int(row, column), do: row * @fabric_side + column + 1
+  test "result", do: assert overlapped_tiles("inputs/input.txt") == 117505
 end
---
+++ 2018/Day_03/part_2.exs
@@ -0,0 +1,30 @@
+defmodule AdventOfCode2018.Day03.Part2 do
+  alias AdventOfCode2018.Day03.Part1
+  alias AdventOfCode2018.Day03.Claims
+  alias AdventOfCode2018.Day03.Fabric
+
+  def intact_claim(file_path) do
+    claims = Claims.extract(file_path)
+    fabric = Part1.fabric_occupation(claims)
+
+    claims
+    |> Enum.find(nil, &is_intact?(&1, fabric))
+    |> Access.get(:id)
+  end
+
+  defp is_intact?(claim, fabric) do
+    claim
+    |> Fabric.occuped_space
+    |> Enum.all?(fn tile -> fabric[tile] == 1 end)
+  end
+end
+
+ExUnit.start
+
+defmodule AdventOfCode2018.Day03.Test2 do
+  use ExUnit.Case
+
+  import AdventOfCode2018.Day03.Part2
+  
+  test "result", do: assert intact_claim("inputs/input.txt") == 1254
+end
