fb89fdb39a07030e56a774df86216da3e99ddcc1
Alex Gascón <alexgascon.93@gmail.com>
2018-12-06 11:00:50 +0100

    Refactoring

--- 2018/Day_05/part_1.exs
+++ 2018/Day_05/part_1.exs
@@ -1,38 +1,17 @@
 defmodule AdventOfCode2018.Day05.Part1 do
   alias AdventOfCode2018.Utils
+  alias AdventOfCode2018.Day05.Polymer
 
   def alchemical_reduction(file_path \\ "inputs/input.txt") do
     file_path
     |> Utils.read_char_lists!
     |> Enum.at(0)
-    |> reduce_polymer
+    |> Polymer.reduce
   end
-
-  def reduce_polymer(polymer) do
-    reduction(polymer, [])
-    |> Enum.reverse
-  end
-
-  defmacro is_opposite_polarity?(unit1, unit2) do
-    quote do: (unquote(unit1) + 32 == unquote(unit2)) or (unquote(unit2) + 32 == unquote(unit1))
-  end
-
-  def reduction([], accumulated_polymer), do: accumulated_polymer
-  def reduction([unit | []], accumulated_polymer), do: [unit] ++ accumulated_polymer
-  def reduction([unit | [next_unit | polymer]], accumulated_polymer) when is_opposite_polarity?(unit, next_unit) do
-    reduce_accumulated_polymer(polymer, accumulated_polymer)
-  end
-  def reduction([unit | polymer], accumulated_polymer), do: reduction(polymer, [unit] ++ accumulated_polymer)
-
-
-  def reduce_accumulated_polymer([next | t], [last | t2]) when is_opposite_polarity?(next, last) do
-    reduce_accumulated_polymer(t, t2)
-  end
-  def reduce_accumulated_polymer(polymer, accumulated_polymer), do: reduction(polymer, accumulated_polymer)
 end
 
 ExUnit.start
-AdventOfCode2018.Day05.Part1.alchemical_reduction |> Enum.count |> IO.inspect
+AdventOfCode2018.Day05.Part1.alchemical_reduction |> Enum.count |> IO.inspect(label: "RESULT")
 
 defmodule AdventOfCode2018.Day05.Test1 do
   use ExUnit.Case
---
+++ 2018/Day_05/utils.ex
@@ -0,0 +1,34 @@
+defmodule AdventOfCode2018.Day05.Polymer do
+
+  def reduce(polymer) do
+    polymer
+    |> reduction([])
+    |> Enum.reverse
+  end
+
+  defmacro is_opposite_polarity?(unit1, unit2) do
+    quote do: (unquote(unit1) + 32 == unquote(unit2)) or (unquote(unit2) + 32 == unquote(unit1))
+  end
+
+  def reduction([], accumulated_polymer) do
+    accumulated_polymer
+  end
+
+  def reduction([unit | [next_unit | polymer]], accumulated_polymer) when is_opposite_polarity?(unit, next_unit) do
+    {polymer, accumulated_polymer} = reduce_accumulated_polymer(polymer, accumulated_polymer)
+    
+    reduction(polymer, accumulated_polymer)
+  end
+
+  def reduction([unit | polymer], accumulated_polymer) do 
+    reduction(polymer, [unit] ++ accumulated_polymer)
+  end
+
+  def reduce_accumulated_polymer([next | upcoming], [last | accumulated]) when is_opposite_polarity?(next, last) do
+    reduce_accumulated_polymer(upcoming, accumulated)
+  end
+
+  def reduce_accumulated_polymer(polymer, accumulated_polymer) do
+    {polymer, accumulated_polymer}
+  end
+end
