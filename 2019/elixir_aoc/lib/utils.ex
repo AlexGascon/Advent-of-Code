230e304b13e6166f3fc228750aadd66d47b866f3
Alex Gascón <alexgascon.93@gmail.com>
2019-12-03 01:18:24 +0000

    Adding Utils functions to read files containing a comma-separated list

--- 2019/elixir_aoc/lib/utils.ex
+++ 2019/elixir_aoc/lib/utils.ex
@@ -16,4 +16,16 @@ defmodule AdventOfCode2019.Utils do
     |> read_lines!
     |> Stream.map(&String.to_charlist(&1))
   end
+
+  def read_list(file_path) do
+    file_path
+    |> File.read!
+    |> String.split(",")
+  end
+
+  def read_int_list(file_path) do
+    file_path
+    |> read_list
+    |> Stream.map(&String.to_integer/1)
+  end
 end
