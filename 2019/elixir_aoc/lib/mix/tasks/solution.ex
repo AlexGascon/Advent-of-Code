fdce8f0af4984f07a6ea3eb92b14c34226753c6b
Alex Gascón <alexgascon.93@gmail.com>
2019-12-04 22:42:36 +0000

    Improving Solution mix task - Getting both solutions
    Improving the Solution mix task to get the solution for both parts of
    that day's problem

--- 2019/elixir_aoc/lib/mix/tasks/solution.ex
+++ 2019/elixir_aoc/lib/mix/tasks/solution.ex
@@ -6,7 +6,8 @@ defmodule Mix.Tasks.Solution do
     case run_tests do
       :ok ->
         day = args |> Enum.at(0) |> String.to_integer
-        {:ok, obtain_solution(day)}
+        [part1_solution, part2_solution] = obtain_solution(day)
+        {:ok, ["Part 1": part1_solution, "Part 2": part2_solution]}
       error ->
         {:error, error}
     end
@@ -15,18 +16,22 @@ defmodule Mix.Tasks.Solution do
 
   defp obtain_solution(day) do
     day
-    |> get_module
-    |> apply(:solution, [filename(day)])
+    |> get_modules
+    |> Enum.map(&apply(&1, :solution, [filename(day)]))
   end
 
-  def get_module(day) do
+  def get_modules(day) do
+    [get_module(day, 1), get_module(day, 2)]
+  end
+
+  def get_module(day, part) do
     day
-    |> module_name
+    |> module_name(part)
     |> String.to_atom
   end
 
-  defp module_name(day) when day < 10, do: "Elixir.AdventOfCode2019.Day0#{day}.Part1"
-  defp module_name(day), do: "Elixir.AdventOfCode2019.Day#{day}.Part1"
+  defp module_name(day, part) when day < 10, do: "Elixir.AdventOfCode2019.Day0#{day}.Part#{part}"
+  defp module_name(day, part), do: "Elixir.AdventOfCode2019.Day#{day}.Part#{part}"
 
   defp filename(day) when day < 10, do: "lib/day_0#{day}/inputs/input.txt"
   defp filename(day), do: "lib/day_#{day}/inputs/input.txt"
