1ac86208c86ede76188c0f1ecfbdccad3bf4a309
Alex Gascón <alexgascon.93@gmail.com>
2020-12-19 19:51:04 +0000

    2020 - Dag 13 Part 2

--- 2020/src/main/kotlin/Day13Part2.kts
+++ 2020/src/main/kotlin/Day13Part2.kts
@@ -1,54 +1,90 @@
 import java.io.File
-import java.sql.Timestamp
 
 val FILENAME = "src/main/kotlin/day13/input.txt"
 val FILENAME_TEST = "src/main/kotlin/day13/input-test.txt"
 val FILENAME_TEST_ADDITIONAL = "src/main/kotlin/day13/input-test-additional.txt"
 val FILENAME_TEST_SIMPLE = "src/main/kotlin/day13/input-test-simple.txt"
 
-fun parseInput(filename: String): List<Pair<Int, Int>> {
+data class BusRequirement(val busLine: Long, val timeOffset: Long)
+
+fun parseInput(filename: String): List<BusRequirement> {
     val (_, busList) = File(filename).readLines()
 
     return busList
         .split(",")
-        .mapIndexed { index, value -> Pair(value, index) }
-        .filterNot { it.first == "x" }
-        .map { pair -> Pair(pair.first.toInt(), pair.second) }
-}
+        .mapIndexed { index, busLineString ->
+            if (busLineString == "x") return@mapIndexed null
 
-fun timeToNextBus(timestamp: Long, busNumber: Int, timeOffset: Int) : Long {
-    val x = (timestamp + timeOffset) % busNumber
-    return if (x == 0L) 0L else (busNumber - x)
-}
-
-fun findSolution(busesAndOffsets: List<Pair<Int,Int>>): Long {
-    var currentTimestamp = 100000000000000L
-    var isSolution = false
-
-    while (!isSolution) {
-        val maxDifference: Long = busesAndOffsets
-            .map { (bus, offset) -> Pair(bus, timeToNextBus(currentTimestamp, bus, offset)) }
-            .maxBy { it.second }!!
-            .second
-
-        if (maxDifference == 0L) {
-            isSolution = true
-        } else {
-            currentTimestamp += maxDifference
+            val busLine = busLineString.toLong()
+            val timeOffset = if (index == 0) 0 else busLine - index
+            BusRequirement(busLine, timeOffset)
         }
+        .filterNotNull()
+}
+
+fun chineseRemainderTheorem(remaindersAndModulos: List<BusRequirement>): Long {
+    val remainders = remaindersAndModulos.map(BusRequirement::timeOffset)
+    val modulus = remaindersAndModulos.map(BusRequirement::busLine)
+    val modulosProduct = modulus.fold(1L) { acc, l -> acc * l }
+
+    var totalSum = 0L
+
+    for (i in remaindersAndModulos.indices) {
+        val wantedRemainder = remainders[i]
+        val currentModulo = modulus[i]
+
+        var currentSummand = (modulosProduct / currentModulo)
+        val currentRemainder = currentSummand % currentModulo
+        if (currentRemainder != wantedRemainder) {
+            currentSummand *= wantedRemainder * moduloInverse(currentRemainder, currentModulo)
+        }
+
+        totalSum += currentSummand
     }
 
-    return currentTimestamp
+    return (totalSum % modulosProduct)
+}
+
+fun moduloInverse(number: Long, modulo: Long): Long {
+    if (number == 0L) {
+        // In this case, we don't care
+        return 0L
+    }
+
+    var numerator = 1L
+    var denominator = number
+
+    while (true) {
+        // First number that makes the denominator higher than the modulo
+        var multiplier: Long = modulo.div(denominator) + 1L
+
+        // Fraction reduction:
+        // We multiply numerator and denominator by the same number, so result is the same
+        // As we are working in modulo, numerator and denominator have to be reduced
+        numerator = (numerator * multiplier) % modulo
+        denominator = (denominator * multiplier) % modulo
+
+        // When we make the denominator reach 1, the numerator is the inverse
+        if (denominator == 1L) {
+            return numerator
+        }
+    }
+}
+
+fun findSolution(busesAndOffsets: List<BusRequirement>): Long {
+    return chineseRemainderTheorem(busesAndOffsets)
 }
 
 fun test() {
     val busesAndOffsets = parseInput(FILENAME_TEST_ADDITIONAL)
+    println("INPUTS = $busesAndOffsets")
     val solution = findSolution(busesAndOffsets)
     println("SOLUTION = $solution")
 }
 
 fun testTwo() {
     val busesAndOffsets = parseInput(FILENAME_TEST)
+    println("INPUTS = $busesAndOffsets")
     val solution = findSolution(busesAndOffsets)
     println("SOLUTION = $solution")
 }
