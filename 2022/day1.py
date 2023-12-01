801da30935ec50280b4bfe4ae5ce4dd4b9a4f627
Alex Gascon <alexgascon.93@gmail.com>
2023-12-01 20:58:01 +0100

    2022 - D1 P1

---
+++ 2022/day1.py
@@ -0,0 +1,49 @@
+"""
+--- Day 1: Trebuchet?! ---
+Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+
+You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+
+As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+Consider your entire calibration document. What is the sum of all of the calibration values?
+"""
+
+import re
+
+DIGIT_SUBSTRING_REGEX = re.compile("\D*(\d.*\d)\D*|\D*(\d)\D*")
+
+total = 0
+for line in open("inputs/day1.txt").readlines():
+    match = DIGIT_SUBSTRING_REGEX.match(line.strip())
+    print("LINE: " + line.strip())
+    multiple_digit_match = match.group(1)
+    
+    print("MATCH: " + str(match.groups()))
+    print()
+
+    if multiple_digit_match:
+        first_digit = multiple_digit_match[0]
+        last_digit = multiple_digit_match[-1]
+
+        total = total + int(f"{first_digit}{last_digit}")
+    else:
+        single_digit_match = match.group(2)
+        digit = single_digit_match[0]
+        total = total + int(f"{digit}{digit}")
+
+print(f"Result: {total}")
